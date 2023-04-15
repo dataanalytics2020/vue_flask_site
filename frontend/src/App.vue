2fe16ef8030ec12371d6072dfa049f8e88e4e1c6
slotdata2020 <slotdata2020@gmail.com>
2023-04-16 02:39:32 +0900

    veuファイルを修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-menu-item index="home" :route="{ name:'home' }">Home</el-menu-item>
+      <el-menu-item index="about" :route="{ name:'about' }">About</el-menu-item>
+      <el-menu-item>
+        <a href="https://element.eleme.io" target="_blank">Link</a>
+      </el-menu-item>
+    </el-menu>
+    <router-view />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      activeIndex: this.$route.name
+    }
+  }
+}
+</script>
+
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+a {
+  text-decoration: none;
 }
 </style>
